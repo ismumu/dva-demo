2a6102236a294e1d1af0b8760e60586038aeb8f1
ismumu <ismmm@qq.com>
2018-01-26 18:14:19 +0800

    add

---
+++ src/routes/es6/regex.tsx
@@ -0,0 +1,31 @@
+
+import * as React from 'react'
+import * as BlogTemplate from '../../components/blogTemplate/index';
+
+
+
+const source =
+`
+# 常用的正则表达式
+
+\`\`\`js
+
+// 匹配允许0的正整数，但是不允许01， 001格式
+/^([1-9]\d*|[0]{1,1})$/
+
+\`\`\`
+
+
+`;
+
+export default class extends React.Component {
+
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return <BlogTemplate source={source} />;
+	}
+
+}
