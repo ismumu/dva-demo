b58a27dd5f296b765c018805d9b75b5841e98ff9
ismumu <ismmm@qq.com>
2018-03-08 13:49:03 +0800

    add

--- components/layout/app.js
+++ components/layout/app.js
@@ -9,7 +9,7 @@ import 'nprogress/nprogress.css'
 import NProgress from 'nprogress';
 
 
-// import styles from './app.less';
+import './app.css';
 
 
 import {
@@ -37,23 +37,6 @@ class App extends React.Component {
 
 		const { dispatch } = props;
 
-		// top scroll progress indicatorDom
-		// window.onscroll = function () {
-
-		// 	let windowHeight = document.body.clientHeight; // 屏幕高度
-		// 	let bodyHeight =  document.body.scrollHeight; // 内容高度
-		// 	let sHeight = bodyHeight - windowHeight;
-		// 	let scrollY =  window.scrollY // 滚动条距离顶部的高度
-
-		// 	window.requestAnimationFrame(function(){
-		// 		dispatch({
-		// 			type: 'dvaDemoApp/updateState',
-		// 			payload: {
-		// 				progressWidth: Math.max(0, Math.min(1, scrollY / sHeight)) * 100,
-		// 			}
-		// 		})
-		// 	});
-		// }
 
 	}
 
@@ -96,15 +79,15 @@ class App extends React.Component {
 
 		const { dvaDemoApp, children, location, loading } = this.props;
 
-
 		return (
-			<Layout className="layout">
+			<div>
+			<Layout style={{minHeight: '100vh'}}>
 				<Sider
 					collapsible
 					collapsed={false}
 					onCollapse={this.onCollapse}
 				>
-					<div>ismumu's home</div>
+					<div className="logo">ismumu's home</div>
 					<Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
 						<Menu.Item key="1">
 							<Icon type="pie-chart" />
@@ -136,28 +119,23 @@ class App extends React.Component {
 					</Menu>
 				</Sider>
 				<Layout>
-					<Header style={{ background: '#fff', padding: 0 }}>
+					<Header style={{background: '#fff', padding: 0}}>
 						<Icon
 							className="trigger"
 							type={0 ? 'menu-unfold' : 'menu-fold'}
 							onClick={this.toggle}
 						/>
 					</Header>
-					<Content style={{ margin: '0 16px' }}>
-						<div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+					<Content style={{ margin: '16px' }}>
+						<div className="content">
 							Bill is a cat.
             			</div>
 					</Content>
-					<Footer style={{ textAlign: 'center' }}>
-						Ant Design ©2016 Created by Ant UED
-          			</Footer>
+					<Footer style={{textAlign: 'center'}} >Contact Email：ismmm@qq.com | <img src="../../static/images/ghs.png" alt="ghs" /> 浙公网安备 33010402001108号</Footer>
 				</Layout>
-				<style jsx>{`
 
-					.layout {
-						min-height: 100vh;
-					}
-
+			</Layout>
+			<style jsx>{`
 					.logo {
 						height: 32px;
 						background: rgba(255,255,255,.2);
@@ -171,14 +149,6 @@ class App extends React.Component {
 						border-radius: 4px;
 						text-transform: capitalize;
 					}
-
-					.header {
-						background: #fff;
-						padding-left: 0;
-					}
-					.contentWrap {
-						margin: 16px;
-					}
 					.content {
 						padding: 24px;
 						background: #fff;
@@ -188,58 +158,8 @@ class App extends React.Component {
 					.breadcrumb {
 						margin: 16px 0;
 					}
-
-					.footer {
-						text-align: center;
-					}
-
-					.trigger {
-						font-size: 18px;
-						line-height: 64px;
-						padding: 0 24px;
-						cursor: pointer;
-						transition: color .3s;
-					}
-
-					.trigger:hover {
-						color: #1890ff;
-					}
-
-					.progressIndicator {
-						position: fixed;
-						top: 0;
-						left: 0;
-						z-index: 5;
-						height: 3px;
-						background: linear-gradient(to right, rgba(24,144,255,0), rgba(24,144,255,1));
-					}
-
-
-
-						#nprogress .bar {
-							background: #1890ff;
-						}
-						#nprogress .peg {
-							box-shadow: 0 0 10px #1890ff, 0 0 5px #1890ff;
-						}
-
-						#nprogress .spinner-icon {
-							border-top-color: #1890ff;
-							border-left-color: #1890ff;
-						}
-
-						hr {
-							margin: 40px 0;
-							border: 1px dashed #eee;
-						}
-
-						pre {
-							border-radius: 4px;
-						}
-
-
                 `}</style>
-			</Layout>
+			</div>
 		)
 	}
 
